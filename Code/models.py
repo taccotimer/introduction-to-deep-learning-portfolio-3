2a6783ff7c1f55f64b802fe06562bd5d68c78b72
Tim Elflein <tim.elflein@study.thws.de>
2026-06-14 15:24:32 +0200

    - fixed bug, where the cross_entropy_loss received null as input from ResNet18 causing it to throw an error

--- Code/models.py
+++ Code/models.py
@@ -1,8 +1,9 @@
 """
-MAI/IDL SS26 - Final assignment. 
+MAI/IDL SS26 - Final assignment.
 
 MG 6/6/2026
 """
+
 import torch
 import torch.nn as nn
 
@@ -14,17 +15,25 @@ class VGGBlock(nn.Module):
 
     C configuration from Simonyan & Zisserman's VGG paper.
     """
+
     def __init__(self, in_channels, out_channels, num_convs, padding=1):
         super().__init__()
         layers = []
         current_in_channels = in_channels
         for i in range(num_convs):
-            is_config_c_tail = (num_convs == 3 and i == 2)
+            is_config_c_tail = num_convs == 3 and i == 2
             kernel_size = 1 if is_config_c_tail else 3
-            layers.append(nn.Conv2d(current_in_channels, out_channels, kernel_size=kernel_size, padding=padding))
+            layers.append(
+                nn.Conv2d(
+                    current_in_channels,
+                    out_channels,
+                    kernel_size=kernel_size,
+                    padding=padding,
+                )
+            )
             layers.append(nn.BatchNorm2d(out_channels))
             layers.append(nn.ReLU(inplace=True))
-            
+
         layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
         self.block = nn.Sequential(*layers)
 
@@ -34,51 +43,62 @@ class VGGBlock(nn.Module):
 
 class ResBlock(nn.Module):
     """ResBlock with 3x3 convolutions (He et al., 2016)."""
+
     def __init__(self, in_channels, out_channels, activation, stride=1):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=3,
+            stride=stride,
+            padding=1,
+            bias=False,
+        )
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.activation = activation
-        
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+
+        self.conv2 = nn.Conv2d(
+            out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False
+        )
         self.bn2 = nn.BatchNorm2d(out_channels)
 
         # If spatial size shrinks (stride > 1) or channels change, adjust the shortcut
         self.shortcut = nn.Identity()
         if stride != 1 or in_channels != out_channels:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels)
+                nn.Conv2d(
+                    in_channels, out_channels, kernel_size=1, stride=stride, bias=False
+                ),
+                nn.BatchNorm2d(out_channels),
             )
 
     def forward(self, x):
         identity = self.shortcut(x)
         out = self.activation(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
-        out += identity  
+        out += identity
         out = self.activation(out)
         return out
 
 
 class AlexNet(nn.Module):
     """AlexNet (Krizhevsky et al., 2012) adapted for smaller inputs."""
+
     def __init__(self, **kwargs):
         super().__init__()
 
         num_classes = kwargs.get("num_classes", 11)
         drop_rate = kwargs.get("drop_rate", 0.5)
-        
+
         self.features = nn.Sequential(
             nn.Conv2d(3, 48, kernel_size=7, stride=2, padding=3),
             nn.BatchNorm2d(48),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-            
             nn.Conv2d(48, 128, kernel_size=5, padding=2),
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-            
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
@@ -87,7 +107,7 @@ class AlexNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
         )
-        
+
         self.classifier = nn.Sequential(
             nn.Dropout(p=drop_rate),
             nn.Linear(3072, 1024),
@@ -106,6 +126,7 @@ class AlexNet(nn.Module):
 
 class VGG16(nn.Module):
     """VGG16 in C configuration of Simonyan & Zisserman, (2014) adapted for smaller inputs."""
+
     def __init__(self, in_channels, num_classes, **kwargs):
         super().__init__()
 
@@ -116,9 +137,9 @@ class VGG16(nn.Module):
             VGGBlock(64, 128, num_convs=2),
             VGGBlock(128, 256, num_convs=3),
             VGGBlock(256, 512, num_convs=3),
-            VGGBlock(512, 512, num_convs=3)
+            VGGBlock(512, 512, num_convs=3),
         )
-        
+
         self.classifier = nn.Sequential(
             nn.Linear(2048, 1024),
             nn.ReLU(inplace=True),
@@ -126,7 +147,7 @@ class VGG16(nn.Module):
             nn.Linear(1024, 512),
             nn.ReLU(inplace=True),
             nn.Dropout(p=drop_rate),
-            nn.Linear(512, num_classes)
+            nn.Linear(512, num_classes),
         )
 
     def forward(self, x):
@@ -137,36 +158,39 @@ class VGG16(nn.Module):
 
 class ResNet18(nn.Module):
     """ResNet18 (He et al., 2016) adapted for smaller inputs.
-    
+
     activation - flexible activation function to allow experimentation (e.g., ReLU, LeakyReLU, etc.)
     """
+
     def __init__(self, in_channels, num_classes, **kwargs):
         super().__init__()
 
         activation = getattr(nn, activation_str)
 
-        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(
+            in_channels, 64, kernel_size=3, stride=1, padding=1, bias=False
+        )
         self.bn1 = nn.BatchNorm2d(64)
         self.activation = activation(inplace=True)
         print("Using activation function:", self.activation)
-        
+
         self.stage1 = nn.Sequential(
             ResBlock(64, 64, activation(inplace=True), stride=1),
-            ResBlock(64, 64, activation(inplace=True), stride=1)
+            ResBlock(64, 64, activation(inplace=True), stride=1),
         )
         self.stage2 = nn.Sequential(
-            ResBlock(64, 128, activation(inplace=True), stride=2),          
-            ResBlock(128, 128, activation(inplace=True), stride=1)
+            ResBlock(64, 128, activation(inplace=True), stride=2),
+            ResBlock(128, 128, activation(inplace=True), stride=1),
         )
         self.stage3 = nn.Sequential(
             ResBlock(128, 256, activation(inplace=True), stride=2),
-            ResBlock(256, 256, activation(inplace=True), stride=1)
+            ResBlock(256, 256, activation(inplace=True), stride=1),
         )
         self.stage4 = nn.Sequential(
             ResBlock(256, 512, activation(inplace=True), stride=2),
-            ResBlock(512, 512, activation(inplace=True), stride=1)
+            ResBlock(512, 512, activation(inplace=True), stride=1),
         )
-        
+
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.classifier = nn.Linear(512, num_classes)
 
@@ -178,4 +202,4 @@ class ResNet18(nn.Module):
         out = self.stage4(out)
         out = self.avgpool(out)
         out = torch.flatten(out, 1)
-        self.classifier(out)
+        return self.classifier(out)
