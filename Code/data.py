f5740e14858ac548d5dcfcb5eb5d64fee29dda2b
Tim <timelflein@gmx.de>
2026-07-04 16:26:45 +0200

    - fixed a bug, in the data split that the validation set was part of the training set

--- Code/data.py
+++ Code/data.py
@@ -17,8 +17,8 @@ def get_loaders(data, data_path, batch_size, val_split=0.1):
     val_size = int(total_samples * val_split)
     val_start = total_samples - val_size
 
-    train_data = data_dict['train_images']
-    train_labels = data_dict['train_labels']
+    train_data = data_dict['train_images'][:val_start]
+    train_labels = data_dict['train_labels'][:val_start]
     val_data = data_dict['train_images'][val_start:]
     val_labels = data_dict['train_labels'][val_start:]
     
