ec79fba13c44ddfcad4ef8deb2a416a83846a58c
Josian Müller <josian.mueller@gmail.com>
2026-07-09 17:11:43 +0200

    fixed 2 minor issues: wrong parameter name in function and wrong file name in function added SlimAlexNet configurations to the config Now if you run main.py you will run every permutation of models and dataset

--- Code/main.py
+++ Code/main.py
@@ -3,14 +3,14 @@ from training_framework import train_main_runs
 
 if __name__ == "__main__":
     train_main_runs(
-        config_files="config_files/config.json",
+        config_file="config_files/config.json",
         trained_models_dir="trained_models",
         with_seed=True
         
     )
 
     test_models(
-        config_file="config_files/training_config.json",
+        config_file="config_files/testing_config.json",
         trained_models_dir="trained_models",
         with_seed=True
     )
